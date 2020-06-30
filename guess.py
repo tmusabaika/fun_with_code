8bcfb0f99c1c1b2d49cac27e1903684da96aa880
tmusabaika <tmusabaika@gmail.com>
2020-06-30 16:39:54 +0200

    initial commit

---
+++ guess.py
@@ -0,0 +1,30 @@
+import random
+
+def check_guess(response, challenge):
+    if int(response) == challenge:
+        return 0
+    else:
+        return 1
+
+random_num = random.randint(0, 10)
+attempts = 1
+
+print(random_num)
+
+while (attempts < 4):
+    
+    print('\n'+'*'*10)
+    print('Attempt ' +str(attempts) + '\n')
+    guess = input('Guess a number between 0 and 10: \n')
+    result = check_guess(guess, random_num)
+    if result == 0:
+        print('Success!')
+        exit(0)
+    elif result == 1:
+        if attempts == 2:
+            print('One more try')
+        elif attempts < 3:
+            print('Give it another go!')
+        else:
+            print('No more tries. Reload Game')
+    attempts += 1
