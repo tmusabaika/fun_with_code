9bac54b399b7ef99b07fe742b38193a1e6aaf900
tmusabaika <tmusabaika@gmail.com>
2020-07-01 06:00:33 +0200

    add check of how close to correct guess and notify user

--- guess.py
+++ guess.py
@@ -18,9 +18,16 @@ while (attempts < 4):
     guess = input('Guess a number between 0 and 10: \n')
     result = check_guess(guess, random_num)
     if result == 0:
-        print('Success!')
+        print('You guessed it!')
         exit(0)
     elif result == 1:
+        variance = abs((float(guess - random_num)/10)*100)
+        if variance <= 20:
+            print('Very close!')
+        elif variance >= 50:
+            print('You drifted...')
+        else:
+            print('Quite close')
         if attempts == 2:
             print('One more try')
         elif attempts < 3:
