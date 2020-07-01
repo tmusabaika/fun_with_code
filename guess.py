c44fa03a6bd3f549f84bf20a3db2154a04e53cc9
tmusabaika <tmusabaika@gmail.com>
2020-07-01 13:45:40 +0200

    move variance check to a function check_variance()

--- guess.py
+++ guess.py
@@ -6,6 +6,15 @@ def check_guess(response, challenge):
     else:
         return 1
 
+def check_variance(response, challenge):
+    variance = abs((float(response - challenge)/10)*100)
+    if variance <= 10:
+        return 'Very close!'
+    elif variance >= 60:
+        return 'You drifted...'
+    else:
+        return ''
+
 random_num = random.randint(0, 10)
 attempts = 1
 
@@ -14,24 +23,18 @@ print(random_num)
 while (attempts < 4):
     
     print('\n'+'*'*10)
-    print('Attempt ' +str(attempts) + '\n')
+    print('Attempt ' + str(attempts) + '\n')
     guess = input('Guess a number between 0 and 10: \n')
     result = check_guess(guess, random_num)
     if result == 0:
         print('You guessed it!')
         exit(0)
     elif result == 1:
-        variance = abs((float(guess - random_num)/10)*100)
-        if variance <= 20:
-            print('Very close!')
-        elif variance >= 50:
-            print('You drifted...')
-        else:
-            print('Quite close')
+        print(check_variance(guess, random_num))
         if attempts == 2:
             print('One more try')
         elif attempts < 3:
-            print('Nope... Give it another go!')
+            print('Give it another go!')
         else:
-            print('Nope... No more tries. Reload Game')
+            print('No more tries. Reload Game')
     attempts += 1
